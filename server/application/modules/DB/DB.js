a525018aa669e9a6ee896e59117ab6024305a3da
Nmoneta <nmoneta20.2@yandex.ru>
2024-05-17 10:08:34 +0400

    Правильный хост

--- server/application/modules/DB/DB.js
+++ server/application/modules/DB/DB.js
@@ -2,7 +2,7 @@ class DB {
     constructor() {
         const mysql = require("mysql");
         this.connection = mysql.createConnection({
-            host: "MySQL-8.2",
+            host: "localhost",
             user: "root",
             database: "counter_offensive",
             password: null
